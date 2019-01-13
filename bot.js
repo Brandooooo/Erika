898578019c7b9e5e7a1f8ee3b0c8cfb41a0aa5be
Brandon <46638296+Brandooooo@users.noreply.github.com>
2019-01-12 21:17:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.VhMU4qANJfA987lcyCzJ3uRI-2efCHcK);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
